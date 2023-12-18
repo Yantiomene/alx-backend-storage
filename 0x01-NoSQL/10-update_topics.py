1eb4dde5cd6853d315bf698f9c357c83191e44ee
Yaninthé Tiomene <yan2016tiomene@gmail.com>
2023-12-18 13:59:45 -0800

    [Feature] Change school topics

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Python function that changes all topics of a school document based on name"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """Changes all topics of a school document
+    Args:
+         mongo_collection: pymongo collection,
+         name: school name to update,
+         topics: list of topics approached in the school
+    Return:
+          Nothing
+    """
+    mongo_collection.update_one(
+        {"name": name},
+        {"$set": {
+            "topics": topics
+            }
+         })
+    
