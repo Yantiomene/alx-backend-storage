e93edeab6b211e181d58880486dd600371b57e7d
Yaninthé Tiomene <yan2016tiomene@gmail.com>
2023-12-20 18:52:39 +0000

    [Update] Adding method to get value from a key and convert it to original format

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Writing strings to Redis"""
 import redis
-import typing
+from typing import Union, Callable, Optional
 import uuid
 
 
@@ -13,9 +13,34 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: typing.Union[str, bytes, int, float]) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """Generate a random key, store the input data in Redis"""
         key = str(uuid.uuid4())
         self._redis.set(key, data)
 
         return key
+
+    def get(self, key: str,
+            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        """Convert the data back to the desire format"""
+
+        value = self._redis.get(key)
+        if fn:
+            value = fn(value)
+        return value
+
+    def get_str(self, key: str) -> str:
+        """Parametrize Cache.get with the correct conversion function"""
+
+        value = self._redis.get(key)
+        return value.decode("utf-8")
+
+    def get_int(self, key: str) -> int:
+        """Parametrize Cache.get with the correct conversion function"""
+
+        value = self._redis.get(key)
+        try:
+            value = int(value.decode("utf-8"))
+        except Exception:
+            value = 0
+        return value
